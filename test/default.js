95f684a07a52c0e62757fb526953ba5a9626121e
Michael van der Weg <michael@joinbox.com>
2014-06-22 22:40:48 +0200

    added some test

--- test/default.js
+++ test/default.js
@@ -47,5 +47,17 @@
 			var ext = new MyExtension();
 			assert.equal(ext.getResourceEventListeners().length, 1);
 		});
+
+		it('should return the correct structur for eventlistners for the models', function() {
+			var ext = new MyExtension();
+			assert.equal(ext.getModelEventListeners()[0].event, 'beforeSave');
+			assert.equal(ext.getModelEventListeners()[0].listener, 'function () {\n\n\t\t}');
+		});
+
+		it('should return the correct structur for eventlistners for the resources', function() {
+			var ext = new MyExtension();
+			assert.equal(ext.getResourceEventListeners()[0].event, 'beforePrepare');
+			assert.equal(ext.getResourceEventListeners()[0].listener, 'function () {\n\n\t\t}');
+		});
 	});
 	
